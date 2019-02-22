131a05cf9ae15151f255299121de98d1d3fb040c
Jordanmendonca <jordan_mendonca@hotmail.comclearclsclscleargit cleargit config --global user.name Jordan>
2019-02-21 23:43:08 -0300

    Classe Mdr e Classe MdrController criadas e Rest fuincionado

---
+++ DesafioDevBraspag/Controllers/mdrController.cs
@@ -0,0 +1,25 @@
+﻿using DesafioDevBraspag.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DesafioDevBraspag.Controllers
+{
+    public class mdrController : ApiController
+    {
+
+        private static List<Mdr> mdr = new List<Mdr>();
+
+        public List<Mdr> Get()
+        {
+            Mdr Adquirinte = new Mdr("Adquirinte A");
+            List<Mdr> lista = new List<Mdr>();
+            lista.Add(Adquirinte);
+            return lista;
+        }
+
+    }
+}
---
+++ DesafioDevBraspag/Models/mdr.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DesafioDevBraspag.Models
+{
+    public class Mdr
+    {
+        public string Adquirente { get; set; }
+       
+
+        public Mdr(string Adquirente)
+        {
+            this.Adquirente = Adquirente;
+          
+        }
+    }
+}
